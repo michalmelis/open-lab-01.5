836ea864ffe111a45ed3556705774879414e4883
michalmelis <michalmelis.sk@gmail.com>
2019-10-14 09:36:48 +0200

    open-lab-01.5

---
+++ open-lab-01.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace open_lab_01._5
+{
+    class Program
+    {
+        public static bool Reverse(bool boolean)
+        {
+            return !boolean;
+        }
+       
+    }
+
+}
